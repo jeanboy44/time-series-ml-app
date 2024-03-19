8899d93bbc02bef3db544e731e6fff745334f3a3
Jingeol Ryu <jgryu@ls-electric.com>
2024-03-20 02:29:44 +0900

    add prophet mlflow comparison

--- app/prophet_forecast.py
+++ app/prophet_forecast.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
+from prophet import Prophet
+
+# from app.utils import plot_plotly
 import mlflow
-from prophet.plot import plot_components_plotly
-from app_utils import plot_plotly_with_testset
-from mlflow.store.artifact.models_artifact_repo import ModelsArtifactRepository
 from pathlib import Path
-from typing import Dict
-from mlflow.entities.model_registry import ModelVersion
-from mlflow.tracking import MlflowClient
+from prophet.plot import plot_plotly, plot_components_plotly
+from app.utils import plot_plotly_with_testset
+from mlflow.store.artifact.models_artifact_repo import ModelsArtifactRepository
 
 URI = "file:///Users/jeanboy/workspace/time-series-ml-app/notebooks/mlruns"
 
@@ -23,27 +23,19 @@ def prepare_dataset():
     return df_train, df_test
 
 
-def download_model(local_dir, version):
+def download_model(local_dir):
     mlflow.set_tracking_uri(URI)
-    ModelsArtifactRepository(f"models:/prophet_model/{version}").download_artifacts(
+    ModelsArtifactRepository("models:/prophet_model/latest").download_artifacts(
         "", dst_path=local_dir
     )
 
 
-def list_models(model_name="prophet_model"):
-    models_list: Dict[str, ModelVersion] = {}
-    for mv in mlflow.search_model_versions(filter_string=f"name='{model_name}'"):
-        models_list[mv.version] = mv
-
-    return models_list
-
-
-def load_model(version):
+def load_model():
     local_dir = Path("tmp")
     if not local_dir.is_dir():
         local_dir.mkdir(parents=True)
 
-    download_model(local_dir, version)
+    download_model(local_dir)
     return mlflow.prophet.load_model(local_dir)
 
 
@@ -54,9 +46,9 @@ def predict_model(m, periods=365):
 
 
 def plot_results(model, forecast):
-    _, df_test = prepare_dataset()
+
     st.subheader("Plot Prediction")
-    st.plotly_chart(plot_plotly_with_testset(model, forecast, df_test))
+    st.plotly_chart(plot_plotly(model, forecast))
     st.subheader("Plot Components")
     st.plotly_chart(plot_components_plotly(model, forecast))
 
@@ -68,38 +60,33 @@ def plot_comparison(model, forecast):
     )
 
 
-def show_results(mv):
-    model = load_model(mv.version)
+def show_results():
+    model = load_model()
     forecast = predict_model(model)
     plot_results(model, forecast)
 
 
-def show_comparision(mv):
-    client = MlflowClient()
-    run_info = client.get_run(mv.run_id)
+def show_comparision():
+    rmse_ = 0.12411
     with st.chat_message("ai"):
-        st.write(f"RMSE: {run_info.data.metrics['rmse']}")
-    model = load_model(mv.version)
+        st.write(f"RMSE: {rmse_}")
+    model = load_model()
     forecast = predict_model(model)
     plot_comparison(model, forecast)
 
 
-def main():
+def prophet_forecast():
     st.sidebar.markdown("----")
     menu = ["Show Results", "Compare Model"]
     choice = st.sidebar.selectbox("Choose", menu)
-    models_list = list_models()
-    if st.button("Refresh"):
-        models_list = list_models()
 
     if choice == "Show Results":
-        model_version = st.selectbox("model version", models_list.keys())
-        show_results(mv=models_list.get(model_version))
+        show_results()
     if choice == "Compare Model":
         col1, col2 = st.columns(2)
         with col1:
-            model_a_version = st.selectbox("model A", models_list.keys())
-            show_comparision(mv=models_list.get(model_a_version))
+            st.selectbox("model A", ["model1", "model2"])
+            show_comparision()
         with col2:
-            model_b_version = st.selectbox("model B", models_list.keys())
-            show_comparision(mv=models_list.get(model_b_version))
+            st.selectbox("model B", ["model1", "model2"])
+            show_comparision()
